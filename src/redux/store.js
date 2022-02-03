db4635c9720960dd00ec22c5e06ee0e2f886ea81
LiViS-code <81714297+LiViS-code@users.noreply.github.com>
2022-02-03 17:33:42 +0200

    upd

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers } from "redux";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    items: combineReducers.items,
+    filter: combineReducers.filter,
+  },
 });
